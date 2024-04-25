e85ba69d6a1da8b4fa641899375195e2443afbab
terryyyz <duchuy.124dk@gmail.com>
2024-04-25 21:03:09 +0700

    sync

---
+++ src/KeyStore.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import "@account-abstraction/contracts/core/BaseAccount.sol";
+import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Create2.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+import "./libraries/CustomERC1967.sol";
+
+import "./interfaces/IKeyStore.sol";
+
+/**
+ * @title Ziphius Keystore
+ * @author Terry
+ * @notice Ziphius Keystore, this smart contract will be deploy to Ethereum only
+ */
+contract KeyStore is IKeyStore, Initializable {
+    address public immutable factory;
+    address public immutable SENTINAL_ADDRESS = address(0x1);
+    bytes32 public immutable WALLET_BYTE_CODE_HASH = keccak256(abi.encodePacked(
+        type(CustomERC1967).creationCode,
+        ""
+    ));
+
+    mapping(address => address) private _keys;
+    uint256 internal _keyCount;
+
+    event SetKey(address key, bool isActives);
+
+    constructor() {
+        factory = msg.sender;
+    }
+
+    function init(address initKey) external override initializer {
+        _keys[SENTINAL_ADDRESS] = initKey;
+        _keys[initKey] = SENTINAL_ADDRESS;
+
+        _keyCount++;
+    }
+
+    /**
+     * @notice only accept wallet
+     */
+    function _requireFromWallet(uint256 walletIndex) internal view {
+        bytes32 salt = keccak256(abi.encode(address(this), walletIndex));
+        address wallet = Create2.computeAddress(salt, WALLET_BYTE_CODE_HASH, factory);
+
+        require(wallet == msg.sender, "KeyStore: Caller is not wallet");
+    }
+
+    function _addKey(address key) internal {
+        require(key != address(0) && key != SENTINAL_ADDRESS && key != address(this), "KeyStore: Invalid Key");
+        _keys[key] = _keys[SENTINAL_ADDRESS];
+        _keys[SENTINAL_ADDRESS] = key;
+        _keyCount++;
+    }
+
+    function _removeKey(address prevKey, address key) internal {
+        require(key != address(0) && key != SENTINAL_ADDRESS, "KeyStore: Invalid Key");
+        require(_keys[prevKey] == key, "KeyStore: Invalid prevKey");
+        _keys[prevKey] = _keys[key];
+        _keys[key] = address(0);
+        _keyCount--;
+    }
+
+    function addKey(address key, uint256 walletIndex) external override {
+        _requireFromWallet(walletIndex);
+        _addKey(key);
+
+        emit SetKey(key, true);
+    }
+
+    function removeKey(address prevKey, address key, uint256 walletIndex) external override {
+        _requireFromWallet(walletIndex);
+        _removeKey(prevKey, key);
+
+        emit SetKey(key, false);
+    }
+
+    function getKeys() external view override returns (address[] memory keys) {
+        address[] memory array = new address[](_keyCount);
+
+        // populate return array
+        uint256 index = 0;
+        address currentKey = _keys[SENTINAL_ADDRESS];
+        while (currentKey != SENTINAL_ADDRESS) {
+            array[index] = currentKey;
+            currentKey = _keys[currentKey];
+            index++;
+        }
+        return array;
+    }
+
+    function isValidKey(address key) external view override returns (bool) {
+        return _keys[key] != address(0) && key != SENTINAL_ADDRESS;
+    }
+}
--- src/WalletFactory.sol
+++ src/WalletFactory.sol
@@ -1,2 +1,75 @@
 // SPDX-License-Identifier: Apache
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Create2.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+
+import "./libraries/CustomERC1967.sol";
+import "./KeyStore.sol";
+
+import "./EthereumWallet.sol";
+
+/**
+ * @title Ethereum Wallet Factory
+ * @author Terry
+ * @notice wallet factory use to create new wallet base on our custom ERC1967Proxy
+ */
+contract EthereumWalletFactory {
+    EthereumWallet public immutable walletImplement;
+    KeyStore public immutable keyStoreImplement;
+
+    constructor(address entryPoint) {
+        walletImplement = new EthereumWallet(entryPoint);
+        keyStoreImplement = new KeyStore();
+    }
+
+    function _createKeyStore(address initKey, bytes32 salt) internal returns (KeyStore) {
+        address payable keyStoreAddress = getKeyStoreAddress(salt);
+        uint codeSize = keyStoreAddress.code.length;
+        if (codeSize > 0) {
+            return KeyStore(keyStoreAddress);
+        }
+
+        // using Clones proxy to saving gas cost
+        Clones.cloneDeterministic(address(keyStoreImplement), salt);
+        KeyStore(keyStoreAddress).init(initKey);
+
+        return KeyStore(keyStoreAddress);
+    }
+
+    function _createWallet(address keyStore, uint256 walletIndex) internal returns (EthereumWallet) {
+        address payable walletAddress = getWalletAddress(keyStore, walletIndex);
+        uint codeSize = walletAddress.code.length;
+        if (codeSize > 0) {
+            return EthereumWallet(walletAddress);
+        }
+
+        bytes32 salt = keccak256(abi.encode(keyStore, walletIndex));
+        new CustomERC1967{ salt: salt }();
+        CustomERC1967(walletAddress).init((address(walletImplement)), abi.encodeCall(EthereumWallet.init, (keyStore)));
+
+        return EthereumWallet(walletAddress);
+    }
+
+    function createWalletWithKeyStore(address keyStore, uint256 walletIndex) external returns (EthereumWallet) {
+        return _createWallet(keyStore, walletIndex);
+    }
+
+    function createWallet(address initKey, uint256 walletIndex, bytes32 keyStoreSalt) external returns (EthereumWallet) {
+        KeyStore keyStore = _createKeyStore(initKey, keyStoreSalt);
+        return _createWallet(address(keyStore), walletIndex);
+    }
+
+    function getWalletAddress(address keyStore, uint256 walletIndex) public view returns (address payable) {
+        bytes32 salt = keccak256(abi.encode(keyStore, walletIndex));
+        return payable(Create2.computeAddress(salt, keccak256(abi.encodePacked(
+            type(CustomERC1967).creationCode,
+            ""
+        ))));
+    }
+
+    function getKeyStoreAddress(bytes32 salt) public view returns (address payable) {
+        return payable(Clones.predictDeterministicAddress(address(keyStoreImplement), salt));
+    }
+}
