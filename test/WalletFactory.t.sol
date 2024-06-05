4dbfb6852f9483fa7efebc7b9fe91963b26b7ff2
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-05 23:16:32 +0700

    Test factory

---
+++ test/WalletFactory.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import "account-abstraction/core/EntryPoint.sol";
+import "account-abstraction/interfaces/IEntryPoint.sol";
+import "openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
+import "../src/WalletFactory.sol";
+import "../src/Wallet.sol";
+import "./ERC4337Utils.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+using ERC4337Utils for EntryPoint;
+
+contract WalletFactoryTest is Test {
+    function setUp() external {
+    }
+
+    function testLog() external {
+        console.logBytes(type(ERC1967Proxy).creationCode);
+    }
+}
