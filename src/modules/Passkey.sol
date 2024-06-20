4581eb5b1e451ab2ed9bc8ebe9c54c74f96e97e2
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-20 22:43:21 +0700

    passkey module

--- src/modules/Passkey.sol
+++ src/modules/Passkey.sol
@@ -4,19 +4,24 @@ pragma solidity ^0.8.0;
 import "../interfaces/IModule.sol";
 import "../interfaces/IWallet.sol";
 
-import {WebAuthn} from "webauthn-sol/WebAuthn.sol";
+import {WebAuthn} from "../libraries/WebAuthn.sol";
 
 contract PasskeyModuleFactory {
-    function create(bytes32 salt) external returns (PasskeyModule) {
-        return new PasskeyModule{salt: salt}();
+    function create(uint256 x, uint256 y, bytes32 salt) external returns (PasskeyModule) {
+        return new PasskeyModule{salt: salt}(x, y);
     }
 }
 
 contract PasskeyModule is IModule {
-    constructor() {
+    uint256 immutable public x;
+    uint256 immutable public y;
+
+    constructor(uint256 inputX, uint256 inputY) {
+        x = inputX;
+        y = inputY;
     }
 
-    function validateUserOp(UserOperation calldata userOp, bytes32)
+    function validateUserOp(UserOperation calldata userOp, bytes32 digest)
         external
         view
         override
@@ -24,14 +29,26 @@ contract PasskeyModule is IModule {
     {
         address module = address(bytes20(userOp.signature[:20]));
         require(module == address(this), "invalid module");
-        return 0;
+
+        bytes memory signature = userOp.signature[20:];
+
+        if (this.isValidSignature.selector == isValidSignature(digest, signature)) {
+            return 0;
+        }
+
+        return 1;
     }
 
-    function callback(UserOperation calldata userOp, bytes32) external override {
+    function callback(UserOperation calldata, bytes32) external override {}
 
-    }
+    function isValidSignature(bytes32 digest, bytes memory signature) public view override returns (bytes4 magicValue) {
+        WebAuthn.WebAuthnAuth memory auth = abi.decode(signature, (WebAuthn.WebAuthnAuth));
 
-    function isValidSignature(bytes32 digest, bytes calldata signature) public view override returns (bytes4 magicValue) {
-        return 0x0000;
+        if (WebAuthn.verify({challenge: abi.encode(digest), requireUV: false, webAuthnAuth: auth, x: x, y: y})) {
+            return this.isValidSignature.selector;
+        } else {
+            return 0x0000;
+        }
+
     }
 }
