9d60cf78f4287599b0abcc63e2baa54019dee41a
imduchuyyy <duchuy.124dk@gmail.com>
2024-07-12 16:33:51 +0700

    update factory

--- script/BaseDeployer.sol
+++ script/BaseDeployer.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 import "../src/GenericFactory.sol";
 
 contract BaseDeployer {
-    GenericFactory public genericFactory = GenericFactory(0x9aE7a293de3D5789Cd24324C167B7BC98A0c70B7);
+    GenericFactory public genericFactory = GenericFactory(0xbC82703bDbE098773059957837016b8CA7374992);
 }
